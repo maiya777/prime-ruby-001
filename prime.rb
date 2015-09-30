742652688081a64d5c7b9d012d3475d2102ca4a3
maiya777 <maiya777@list.ru>
2015-09-30 20:20:24 +0300

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(int)
+  i=2
+  primness=true
+  until i>Math.sqrt(int) do
+    if int%i==0 
+    primness=false
+    break
+    end
+    i+=1
+  end
+  primness
+end# Add  code here!
+
+
+a=prime?(7)
+b=prime?(15)
+c=prime?(137)
+d=prime?(70)
+puts a,b,c,d
